ebb6d80c35d54a0cd0703b888f56b49a97a53699
Leskovets Maksym <leskovecmaksim@gmail.com>
2025-05-11 20:43:09 +0100

    finishes hw01

---
+++ task_1.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+    def __init__(self, make, model, spec):
+        self.make = make
+        self.model = model
+        self.spec = spec
+
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+
+class Car(Vehicle):
+    def start_engine(self):
+        print(f"{self.make} {self.model} ({self.spec}): Двигун запущено")
+
+
+class Motorcycle(Vehicle):
+    def start_engine(self):
+        print(f"{self.make} {self.model} ({self.spec}): Мотор заведено")
+
+
+class VehicleFactory(ABC):
+    @abstractmethod
+    def create_car(self, make, model):
+        pass
+
+    @abstractmethod
+    def create_motorcycle(self, make, model):
+        pass
+
+
+class USVehicleFactory(VehicleFactory):
+    def create_car(self, make, model):
+        return Car(make, model, "US Spec")
+
+    def create_motorcycle(self, make, model):
+        return Motorcycle(make, model, "US Spec")
+
+
+class EUVehicleFactory(VehicleFactory):
+    def create_car(self, make, model):
+        return Car(make, model, "EU Spec")
+
+    def create_motorcycle(self, make, model):
+        return Motorcycle(make, model, "EU Spec")
+
+
+# Використання
+us_factory = USVehicleFactory()
+vehicle1 = us_factory.create_car("Dodge", "SRT")
+vehicle1.start_engine()
+
+vehicle2 = us_factory.create_motorcycle("Harley-Davidson", "Sportster")
+vehicle2.start_engine()
+
+eu_factory = EUVehicleFactory()
+vehicle3 = eu_factory.create_car("Volkswagen", "Golf")
+vehicle3.start_engine()
+
+vehicle4 = eu_factory.create_motorcycle("BMW", "R1200GS")
+vehicle4.start_engine()
